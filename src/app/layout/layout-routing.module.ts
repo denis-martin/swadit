6b52fb5a4e8be1b4e442b046178437d2f54aa09a
denis <denis@froschbach.io>
2021-08-28 22:55:06 +0200

    removed parameters and responses modules (integrated into definitions)

--- src/app/layout/layout-routing.module.ts
+++ src/app/layout/layout-routing.module.ts
@@ -13,8 +13,6 @@ const routes: Routes = [
 			
 			{ path: 'api-info', loadChildren: () => import('./api-info/api-info.module').then(m => m.ApiInfoModule) },
 			{ path: 'definitions', loadChildren: () => import('./definitions/definitions.module').then(m => m.DefinitionsModule) },
-			{ path: 'parameters', loadChildren: () => import('./parameters/parameters.module').then(m => m.ParametersModule) },
-			{ path: 'responses', loadChildren: () => import('./responses/responses.module').then(m => m.ResponsesModule) },
 			{ path: 'paths', loadChildren: () => import('./paths/paths.module').then(m => m.PathsModule) },
 			{ path: 'path/:path', loadChildren: () => import('./path/path.module').then(m => m.PathModule) },
 			{ path: 'path/:path/:method', loadChildren: () => import('./path/path.module').then(m => m.PathModule) },
